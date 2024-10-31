4769442cf87a2efb31edf362ae32cbb8cf4a7128
Cauê Moreira <162379957+Caue06@users.noreply.github.com>
2024-10-31 20:45:42 -0300

    1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import ListaDeContato from './components/ListaDeContato'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <ListaDeContato/>
+    </>
+  )
+}
+
+export default App
